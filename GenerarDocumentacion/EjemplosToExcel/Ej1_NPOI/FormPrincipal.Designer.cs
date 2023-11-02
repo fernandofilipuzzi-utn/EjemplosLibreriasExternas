998de588d9f7c089ac7a617bb63106393c917ae0
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-11-02 14:55:19 -0300

    formato xls

--- GenerarDocumentacion/EjemplosToExcel/Ej1_NPOI/FormPrincipal.Designer.cs
+++ GenerarDocumentacion/EjemplosToExcel/Ej1_NPOI/FormPrincipal.Designer.cs
@@ -31,6 +31,7 @@ namespace Ej1_NPOI
         {
             this.button2 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
@@ -47,7 +48,7 @@ namespace Ej1_NPOI
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(37, 109);
+            this.linkLabel1.Location = new System.Drawing.Point(35, 229);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(69, 16);
             this.linkLabel1.TabIndex = 24;
@@ -55,11 +56,23 @@ namespace Ej1_NPOI
             this.linkLabel1.Text = "linkLabel1";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 145);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 61);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Imprimir recibos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 134);
+            this.ClientSize = new System.Drawing.Size(472, 263);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button2);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -79,6 +92,7 @@ namespace Ej1_NPOI
         #endregion
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GenerarDocumentacion/EjemplosToExcel/Ej1_NPOI/FormPrincipal.cs
+++ GenerarDocumentacion/EjemplosToExcel/Ej1_NPOI/FormPrincipal.cs
@@ -43,7 +43,7 @@ namespace Ej1_NPOI
             {
                 IWorkbook wb = new XSSFWorkbook();
                 var sheet = wb.CreateSheet("hoja 1");
-                
+
                 ICellStyle style = wb.CreateCellStyle();
 
                 # region fuente Set font style
@@ -62,7 +62,7 @@ namespace Ej1_NPOI
 
                 #region cabecera
                 IRow headerRow = sheet.CreateRow(nroFila);
-                
+
 
                 int nroColumna = 0;
 
@@ -71,7 +71,7 @@ namespace Ej1_NPOI
                 headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
                 headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
                 headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
-                
+
 
                 headerRow.CreateCell(nroColumna);
                 headerRow.GetCell(nroColumna).SetCellValue("NOMBRE");
@@ -193,6 +193,179 @@ namespace Ej1_NPOI
                 {
                     System.Diagnostics.Process.Start(path);
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Persona> personas = new List<Persona>
+            {
+                new Persona{ Dni=40122312,Nombre="Juan", Nacimiento=new DateTime(2000,02,1,3,4,5)},
+                new Persona{ Dni=23142512,Nombre="Giuliana", Nacimiento=new DateTime(2000,12,12,3,4,5) },
+                new Persona{ Dni=36322312,Nombre="Regina", Nacimiento=new DateTime(1995,1,12,6,7,8) },
+                new Persona{ Dni=37332212,Nombre="Natalia", Nacimiento=new DateTime(1995,1,12,6,7,8) }
+            };
+
+            try
+            {
+                //formato excel 97-2003
+                IWorkbook wb = new HSSFWorkbook();
+                var sheet = wb.CreateSheet("hoja 1");
+
+                ICellStyle style = wb.CreateCellStyle();
+
+                # region fuente Set font style
+                IFont hFont = wb.CreateFont();
+                hFont.FontName = "Arial";
+                hFont.IsBold = true;
+                hFont.FontHeightInPoints = 10;
+
+                IFont font = wb.CreateFont();
+                font.FontName = "Arial";
+                font.IsBold = false;
+                font.FontHeightInPoints = 10;
+                #endregion
+
+                int nroFila = 0;
+
+                #region cabecera
+                IRow headerRow = sheet.CreateRow(nroFila);
+
+
+                int nroColumna = 0;
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("DNI");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("NOMBRE");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("FECHA NACIMIENTO");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("HORA NACIMIENTO");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("Valor");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+
+                headerRow.CreateCell(nroColumna);
+                headerRow.GetCell(nroColumna).SetCellValue("Moneda");
+                headerRow.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                headerRow.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                headerRow.GetCell(nroColumna++).CellStyle.SetFont(hFont);
+                #endregion
+
+                foreach (Persona p in personas)
+                {
+                    var row = sheet.CreateRow(++nroFila);
+                    nroColumna = 0;
+                    //
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).SetCellType(CellType.Numeric);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    //el formato que toma es por ejemplo: 12.232.212
+                    //con @ tomaría la configuración regional
+                    //row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("###,###,###;@");
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("###,###,###");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(p.Dni);
+                    //
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).SetCellType(CellType.String);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("text");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(p.Nombre);
+                    //
+                    // forma 1 - formato forzado
+                    //row.CreateCell(nroColumna);
+                    //row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    //row.GetCell(nroColumna).CellStyle.DataFormat=HSSFDataFormat.GetBuiltinFormat("dd/mm/yyyy");
+                    //row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    //row.GetCell(nroColumna++).SetCellValue($"{p.Nacimiento:d/M/yyyy}");
+                    //
+                    // form 2 - la celda toma el formato de celda con el tipo fecha y el formato especificado
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    //formato año largo
+                    //row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("dd/mm/yyyy");
+                    //formato año corto
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("dd/mm/yy");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(p.Nacimiento);
+                    //
+                    // forma 1
+                    //row.CreateCell(nroColumna);
+                    //row.GetCell(nroColumna).SetCellType(CellType.String);
+                    //row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    //row.GetCell(nroColumna++).SetCellValue($"{p.Nacimiento:HH:mm:ss}");
+                    //
+                    // forma 2
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    //row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("h:m:s");
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("hh:mm:ss");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(p.Nacimiento);
+                    //
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).SetCellType(CellType.Numeric);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("0.00");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(0.12);
+                    //
+                    //moneda
+                    row.CreateCell(nroColumna);
+                    row.GetCell(nroColumna).SetCellType(CellType.Numeric);
+                    row.GetCell(nroColumna).CellStyle = wb.CreateCellStyle();
+                    row.GetCell(nroColumna).CellStyle.DataFormat = wb.CreateDataFormat().GetFormat("$#,##0.00");
+                    row.GetCell(nroColumna).CellStyle.SetFont(font);
+                    row.GetCell(nroColumna++).SetCellValue(0.12);
+                }
+
+                #region rediminesionando las columnas
+                int numberOfColumns = 6;//sheet.GetRow(nroColumna - 1).PhysicalNumberOfCells;
+                for (int i = 0; i <= numberOfColumns; i++)
+                {
+                    sheet.AutoSizeColumn(i);
+                }
+                #endregion
+
+                byte[] bytes = new byte[0];
+                using (var ms = new MemoryStream())
+                {
+                    wb.Write(ms);
+                    bytes = ms.ToArray();
+                }
+
+                GC.Collect();
+
+                string filePath = "../../Salida/mi_archivo.xls";
+                File.WriteAllBytes(filePath, bytes);
+
+                linkLabel1.Text = Path.GetFullPath(filePath);
             }
             catch (Exception ex)
             {
