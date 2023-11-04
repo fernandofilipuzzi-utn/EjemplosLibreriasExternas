ef2509d1cbec53f39822d91b0965f0a25ac00963
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-11-04 02:40:05 -0300

    mejorando ejemplos, valores double

--- GenerarDocumentacion/EjemplosToExcel/EPPlus_excel_ClassLib/Models/Ejemplo.cs
+++ GenerarDocumentacion/EjemplosToExcel/EPPlus_excel_ClassLib/Models/Ejemplo.cs
@@ -18,12 +18,12 @@ namespace EPPlus_excel_ClassLib.Models
         {
             List<Ejemplo> personas = new List<Ejemplo>
             {
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 1", Fecha=new DateTime(1995,3,1,0,0,0)},
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 2", Fecha=new DateTime(1995,2,1,3,4,5)},
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 3", Fecha=new DateTime(1920,4,3,6,7,8)},
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 4", Fecha=new DateTime(2020,5,6,9,10,11)},
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 5", Fecha=new DateTime(2022,7,8,12,13,14)},
-                new Ejemplo{ Numero=40122312, Texto="Ejemplo 6", Fecha=new DateTime(2003,9,10,15,16,17)}
+                 new Ejemplo{ Numero=40122312, Texto="Ejemplo 1", Fecha=new DateTime(1995,3,1,0,0,0), Moneda=00.0},
+                new Ejemplo{ Numero=40122312, Texto="Ejemplo 2", Fecha=new DateTime(1995,2,1,3,4,5), Moneda=0.2},
+                new Ejemplo{ Numero=40122312, Texto="Ejemplo 3", Fecha=new DateTime(1920,4,3,6,7,8), Moneda=1.0},
+                new Ejemplo{ Numero=40122312, Texto="Ejemplo 4", Fecha=new DateTime(2020,5,6,9,10,11), Moneda=1.2},
+                new Ejemplo{ Numero=40122312, Texto="Ejemplo 5", Fecha=new DateTime(2022,7,8,12,13,14), Moneda=23423.2},
+                new Ejemplo{ Numero=40122312, Texto="Ejemplo 6", Fecha=new DateTime(2003,9,10,15,16,17), Moneda=23423.2}
             };
             return personas;
         }
